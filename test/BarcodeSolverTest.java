4413af46c79486ba4a6ad444effe52442513630b
CamPlume1 <plume.c@northeastern.edu>
2023-12-27 08:15:25 -0800

    Add test cases, documentation for Zero Encrypt

--- test/BarcodeSolverTest.java
+++ test/BarcodeSolverTest.java
@@ -50,7 +50,7 @@ public class BarcodeSolverTest {
     }
 
     /**
-     * Tests BASIC strategy
+     * Tests PostProcessing strategy
      */
     @Test
     public void postProcessingSolverTest(){
@@ -87,6 +87,46 @@ public class BarcodeSolverTest {
         //Double reverse + Double Encrypt
         String comparison3 = BarcodeSolver.solveAll(List.of("#!##12#3!4!#59^^#6%7%####"), strategy).get(0);
         assertEquals(comparison3, "123412120667");
+
+    }
+
+
+    /**
+     * Tests Zero Encrypt Strategy
+     */
+    @Test
+    public void zeroEncryptSolverTest(){
+        String baseCorrect = "1221430867";
+        Strategies strategy = Strategies.ZERO_ENCRYPT;
+
+        //SECTION: Basic test cases
+        this.givenTestCases(strategy);
+
+        //SECTION: Try operations in first block, should all be equal,
+        String b1 = BarcodeSolver.solveAll(List.of("#!^%##12#34!#59^#67%####"), strategy).get(0);
+        String b2 = BarcodeSolver.solveAll(List.of("#^##12#34!#59^#67%####"), strategy).get(0);
+        String b3 = BarcodeSolver.solveAll(List.of("#%##12#34!#59^#67%####"), strategy).get(0);
+        String b4 = BarcodeSolver.solveAll(List.of("#!^%1##12#34!#59^#67%####"), strategy).get(0);
+        assertEquals(b1, baseCorrect);
+        assertEquals(b3, baseCorrect);
+        assertEquals(b2, baseCorrect);
+        assertEquals(b4, "1" + baseCorrect);
+
+
+
+        //SECTION: Try doubling up the different operations, make sure they work sequentially
+
+        //double repeat, before each combo
+        String c1  = BarcodeSolver.solveAll(List.of("#!##12#3!4!#59^#67%####"), strategy).get(0);
+        assertEquals(c1, "122142130867");
+        String c2 = BarcodeSolver.solveAll(List.of("#!##12#34!#!59^####"), strategy).get(0);
+        assertEquals(c2, "122143341259");
+
+
+
+        //Double reverse + Double Encrypt: THIS IS WHERE ZERO ENCRYPT DIFFERS FROM BASIC
+        String comparison3 = BarcodeSolver.solveAll(List.of("#!##12#3!4!#59^^#6%7%####"), strategy).get(0);
+        assertEquals(comparison3, "123124120867");
 
     }
 
